71dba98c43b9891eb7ab7ee245102158a5dd701d
hskimse <hskimse1@gmail.com>
2023-07-10 14:35:55 +0900

    v1.03 UI 개선

--- yacht.py
+++ yacht.py
@@ -1,30 +1,25 @@
-#yacht dice game v1.02
+#yacht dice game v1.03
 
 import random
 import os
 
-yrdice = [0, 0, 0, 0, 0]
-rule = ['-' for _ in range(12)]
-
-def roll() :
-	global yrdice
-	for i in range(len(yrdice)) : yrdice[i] = random.randrange(1,7)
-	
-	return yrdice
+yrdice = [0] * 5
+rule = ['-'] * 12
 
 def sheet() :
+	os.system('clear')
 	print("---------------------------------------")
 	print("                 점수표")
-	print("1. Ones                              {}".format(rule[0]))
-	print("2. Twos                              {}".format(rule[1]))
-	print("3. Threes                            {}".format(rule[2]))
-	print("4. Fours                             {}".format(rule[3]))
-	print("5. Fives                             {}".format(rule[4]))
-	print("6. Sixes                             {}".format(rule[5]))
+	print("1.  Ones                             {}".format(rule[0]))
+	print("2.  Twos                             {}".format(rule[1]))
+	print("3.  Threes                           {}".format(rule[2]))
+	print("4.  Fours                            {}".format(rule[3]))
+	print("5.  Fives                            {}".format(rule[4]))
+	print("6.  Sixes                            {}".format(rule[5]))
 	print("")
-	print("7. Choice                            {}".format(rule[6]))
-	print("8. 4 of Kind                         {}".format(rule[7]))
-	print("9. Full House                        {}".format(rule[8]))
+	print("7.  Choice                           {}".format(rule[6]))
+	print("8.  Four of a Kind                   {}".format(rule[7]))
+	print("9.  Full House                       {}".format(rule[8]))
 	print("10. Little Straight                  {}".format(rule[9]))
 	print("11. Big Straight                     {}".format(rule[10]))
 	print("12. Yacht                            {}".format(rule[11]))
@@ -44,11 +39,11 @@ def calc(a) :
 			a = int(a)
 			break
 		except : 
-			print("[sys]잘못 입력하셨습니다.")
+			print("[sys] 잘못 입력하셨습니다.\n")
 			a = input("적용할 규칙을 선택하십시오 >>")
 	
 	if a > 12 or a < 1 :
-		print("[sys]잘못 입력하셨습니다.")
+		print("[sys] 잘못 입력하셨습니다.\n")
 		a = input("적용할 규칙을 선택하십시오 >>")
 		return calc(a)
 	
@@ -104,13 +99,16 @@ def game() :
 		if yourchoice == '0' : quit() #1 > 0(프로그램 종료)
 		
 		if yourchoice == '1' : #1 > 1(주사위 굴리기)
-			print("\n아래가 당신이 굴린 주사위입니다.")
-			print(roll())
+			for i in range(5) : yrdice[i] = random.randrange(1,7)
+			
+			sheet()
+			print("당신의 주사위           {}\n".format(yrdice))
+			
 			print("\n0. 프로그램 종료\n1. 이대로 진행\n2. 변경할 주사위 선택")
 			break
 			
 		else:
-			print("[sys]잘못 입력하셨습니다.")
+			print("[sys] 잘못 입력하셨습니다.\n")
 			
 	while True :
 		yourchoice = input(">> ")
@@ -118,6 +116,7 @@ def game() :
 		
 		if yourchoice == '1' : #1 > 1 > 1(이대로 진행)
 			sheet()
+			print("당신의 주사위           {}\n".format(yrdice))
 			
 			a = input("적용할 규칙을 선택하십시오 >> ")		
 			calc(a)
@@ -138,25 +137,30 @@ def game() :
 			for i in range(2) :
 				temp = 1
 				riddice = []
-		
+				
 				while temp != 0 :
 					try :
-						print("\n변경할 주사위를 선택해주세요(1-5)\n더이상 선택할 주사위가 없으면 0을 눌러주세요.")
+						sheet()
+						print("당신의 주사위           {}\n".format(yrdice))
+						
+						print("\n변경할 주사위 ( ",end = '')
+						print(*sorted(riddice),')를 선택해주세요(1-5)\n더이상 선택할 주사위가 없으면 0을 눌러주세요.')
 						temp = int(input(">> "))
 				
-						if temp > 5 : print("[sys]잘못 입력하셨습니다.")
+						if temp > 5 : print("[sys] 잘못 입력하셨습니다.\n")
 				
 						elif riddice.count(temp) == 1 : print("중복된 숫자를 입력하셨습니다.")
 					
 						elif temp != 0 : riddice.append(temp)
-			
-					except:
-						print("[sys]잘못 입력하셨습니다.")
-				
+						
+					except :
+						print("")
+						
 				for dienum in riddice :
 					yrdice[dienum - 1] = random.randrange(1,7)
-				print("\n아래가 당신이 굴린 주사위입니다.")
-				print(yrdice)
+				
+				sheet()
+				print("당신의 주사위           {}\n".format(yrdice))
 			
 				if i == 0:
 					print("\n0. 프로그램 종료\n1. 이대로 진행\n2. 변경할 주사위 선택")
@@ -164,12 +168,13 @@ def game() :
 					
 					if yourchoice == '0' : quit()
 					if yourchoice == '1' : break
-					
 			break
 					
-		else : print("[sys]잘못 입력하셨습니다.")
+		else : print("[sys] 잘못 입력하셨습니다.\n")
 				
 	sheet()
+	
+	print("당신의 주사위           {}\n".format(yrdice))
 		
 	a = input("적용할 규칙을 선택하십시오 >> ")	
 	calc(a)
@@ -184,9 +189,10 @@ def game() :
 		return
 			
 	else :return game()
-		
+	
+os.system('clear')
 print("---------------------------------------")
-print("              yacht v1.02")
+print("              yacht v1.03")
 print("              made by kim")
 print("---------------------------------------")
 print("0. 프로그램 종료\n1. 게임 시작")
@@ -199,4 +205,4 @@ while True:
 		game()
 		break
 	else:
-		print("[sys]잘못 입력하셨습니다.")
+		print("[sys] 잘못 입력하셨습니다.\n")
